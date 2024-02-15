69cfd7fcb3c3618b44d3b4171c6dae37aabc4302
Ivan <prostoyachelovek@gmail.com>
2024-02-15 19:51:55 +0300

    add termloop and make background

--- go.mod
+++ go.mod
@@ -1,3 +1,10 @@
 module github.com/shalimovSens/dino
 
 go 1.22.0
+
+require (
+	github.com/JoelOtter/termloop v0.0.0-20210806173944-5f7c38744afb // indirect
+	github.com/mattn/go-runewidth v0.0.15 // indirect
+	github.com/nsf/termbox-go v1.1.1 // indirect
+	github.com/rivo/uniseg v0.4.7 // indirect
+)
--- index.go
+++ index.go
@@ -1,7 +1,15 @@
 package main
 
-import "fmt"
+import tl "github.com/JoelOtter/termloop"
 
 func main() {
-	fmt.Println("Dinosaur game")
+	game := tl.NewGame()
+
+	level := tl.NewBaseLevel(tl.Cell{
+		Bg: tl.ColorWhite,
+		Fg: tl.ColorBlack,
+		Ch: ' ',
+	})
+	game.Screen().SetLevel(level)
+	game.Start()
 }
